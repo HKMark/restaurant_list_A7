f7c337fca32ee4632bc86cb781bd274649e13c50
HKMark <HongKongMark@Gmail.com>
2023-01-03 17:33:08 +0800

    feat: add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,11 +2,23 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const todoSchema = new Schema({
+  id: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
   },
+  name_en: {
+    type: String,
+    required: true
+  },
   category: {
+    type: String,
+    required: true
+  },
+  image: {
     type: String,
     required: true
   },
@@ -15,6 +27,14 @@ const todoSchema = new Schema({
     required: true
   },
   phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
     type: String,
     required: true
   },
