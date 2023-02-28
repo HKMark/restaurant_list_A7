804dc946856c0d706a395adcba037160e57f4bc2
Mark Law <hongkongmark@gmail.com>
2023-02-28 19:29:46 +0800

    feat: add user modules and reduce banner height

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const index = require('./modules/index')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 router.use('/', index)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
 
 module.exports = router
