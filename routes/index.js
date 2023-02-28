d268d6a070e4511b589bd4edf955b0342470cb9a
HKMark <HongKongMark@Gmail.com>
2023-03-01 02:07:10 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ const index = require('./modules/index')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
 router.use('/restaurants', authenticator, restaurants)
 router.use('/search', authenticator, search)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, index)
 
 module.exports = router
